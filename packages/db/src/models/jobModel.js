e9a4f2da053bcbd0227bb731c0ee1351c50b5355
Taysa Abinader <ferreira.taysa@gmail.com>
2025-11-16 21:22:08 +0200

    fix(jobModel): adjust jobModel.

--- packages/db/src/models/jobModel.js
+++ packages/db/src/models/jobModel.js
@@ -2,11 +2,10 @@ import mongoose from "mongoose";
 
 const jobSchema = new mongoose.Schema({
   title: String,
+  link: String,
   company: String,
   location: String,
   description: String,
-  skills: [String],
-  source: String,
 }, { timestamps: true });
 
 export const Job = mongoose.model("Job", jobSchema);
