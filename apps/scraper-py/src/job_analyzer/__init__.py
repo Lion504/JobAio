db89bf77e69da0e1323fd9285828c19c3b096bac
Lion504 <lehtonen6677@gmail.com>
2025-11-21 00:01:54 +0200

    refactor(job_analyzer): remove JoblyAnalyzer from exports
    Remove the import and export of JoblyAnalyzer from the job_analyzer module,
    as it is no longer needed in the current implementation.
    
    BREAKING CHANGE: JoblyAnalyzer is no longer available for import from job_analyzer

--- apps/scraper-py/src/job_analyzer/__init__.py
+++ apps/scraper-py/src/job_analyzer/__init__.py
@@ -8,6 +8,5 @@ __version__ = "1.0.0"
 
 from .ai_analyzer import HybridJobAnalyzer
 from .base_analyzer import BaseJobAnalyzer
-from .site_analyzers.jobly_analyzer import JoblyAnalyzer
 
-__all__ = ["BaseJobAnalyzer", "HybridJobAnalyzer", "JoblyAnalyzer"]
+__all__ = ["BaseJobAnalyzer", "HybridJobAnalyzer"]
