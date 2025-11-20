a2b5a9fb9034bd27a74af6852ed298f214677f91
Unai San Segundo <unais@metropolia.fi>
2025-11-20 11:53:11 +0200

    refactor: remove suggestions route from jobRouter
    - Removed the /suggestions endpoint from jobRouter to streamline job retrieval functionality.

--- apps/api/src/routes/jobRouter.js
+++ apps/api/src/routes/jobRouter.js
@@ -4,13 +4,11 @@ const {
   getJobByTitle,
   getJobByCompany,
   getJobByLocation,
-  getSuggestions,
 } = require("../controllers/jobController");
 
 const router = express.Router();
 
 router.get("/", getAllJobs);
-router.get("/suggestions", getSuggestions);
 router.get("/:jobTitle", getJobByTitle);
 router.get("/:jobCompany", getJobByCompany);
 router.get("/:jobLocation", getJobByLocation);
