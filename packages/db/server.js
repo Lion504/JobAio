5d30618eaea462fac81907f3939869a0f672edfe
Unai San Segundo <unais@metropolia.fi>
2025-12-04 19:30:51 +0200

    fix(db): increase base size limit in requests

--- packages/db/server.js
+++ packages/db/server.js
@@ -9,7 +9,8 @@ mongoose.set("strictQuery", true);
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use("/api/jobs", jobRoutes);
 
 mongoose
