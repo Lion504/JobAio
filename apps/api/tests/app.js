f054dc719d5f02f4bf3ef8be278b43a8e4412af1
Lion504 <Lehtonen6677@gmail.com>
2025-12-06 16:17:53 +0200

    chore(test): remove deprecated api test file

--- apps/api/tests/app.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import jobRouter from "./routes/jobRouter";
-
-const app = express();
-app.use(express.json());
-
-app.use("/api/jobs", jobRouter);
-
-export default app;
